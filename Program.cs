6f7bd4371fe8db435c76989ac19db35a1e3a60c1
Vincent Hui <vincenthui@theambassadors.com>
2020-07-19 23:50:00 +0100

    initial commit with git ignore

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace test
+{
+    class Program
+    {
+        //expression body definition for aesthetic style
+        static int getMatchedWordCount(string text, string word) => new Regex($@"\b{word}\b").Matches(text).Count;
+        //get string from txt file in directory of execution
+        static string txtFileContent(string fileName) => File.ReadAllText(Path.Combine(Directory.GetCurrentDirectory(), fileName));
+
+        static void Main(string[] args)
+        {
+            string text = txtFileContent("source.txt");
+            Console.WriteLine("amount of occurances = {0}", getMatchedWordCount(text, args[0]));
+        }
+
+    }
+}
