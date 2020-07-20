f162c8f2a2a4ba263e52b9db6c9b9eaeeea5e909
Vincent Hui <vincenthui@theambassadors.com>
2020-07-20 01:24:12 +0100

    Adding in meat of logic to get top ten

--- Program.cs
+++ Program.cs
@@ -1,25 +1,46 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Text.RegularExpressions;
 
 namespace test
 {
     class Program
     {
-        //expression body definition for aesthetic style
-        static int getMatchedWordCount(string text, string word) => new Regex($@"\b{word}\b").Matches(text).Count;
         //get string from txt file in directory of execution
         static string txtFileContent(string fileName) => File.ReadAllText(Path.Combine(Directory.GetCurrentDirectory(), fileName));
 
+        //use this to test matching logic
+        static int getMatchedWordCount(string text, string word) => new Regex($@"\b{word}\b").Matches(text).Count;
+
+        //feel free to extend to account for escape/special characters 
+        static string[] getSplitWords(string text) => Regex.Split(text, @"\W+");
+
+        //use linq to get a spiffy top ten list
+        static Dictionary<string, int> getTopTen (string text)=>
+            getSplitWords(text)               
+                .GroupBy(word => word)
+                .OrderByDescending(group => group.Count())
+                .Take(10)
+                .ToDictionary(
+                    group => group.Key, 
+                    group => group.Count());
+
         static void Main(string[] args)
         {
-            if (args.Length <= 0) {
-                Console.WriteLine("enter word to search in first argument eg : dotnet test.dll wordToSearch");
-                return;
-            }
             string text = txtFileContent("source.txt");
-            Console.WriteLine("amount of occurances = {0}", getMatchedWordCount(text, args[0]));
+            foreach (var kvp in getTopTen(text)) {
+                Console.WriteLine("{0} : {1}", kvp.Key, kvp.Value);
+            }
         }
 
+        //run this for a quick test of top ten values
+        static void Test(string text) {
+            foreach (var kvp in getTopTen(text))
+            {
+                Console.WriteLine(kvp.Value == getMatchedWordCount(text, kvp.Key) ? "PASS" : "FAIL");
+            }
+        }
     }
 }
